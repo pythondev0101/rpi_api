05bb34be51345a63c1097841bd3830c7eb98098c
pythondev0101 <rmontemayor0101@gmail.com>
2020-09-14 15:37:12 +0800

    Add get_leds_status

--- app/api/recipes_api.py
+++ app/api/recipes_api.py
@@ -16,9 +16,14 @@ def get_recipes():
     # SERIALIZE MODELS
     _recipes_list = []
     for recipe in _recipes:
+        _picklist = []
+        for pick in recipe.recipe_pick:
+            _picklist.append(pick.id)
+
         _recipes_list.append({
             'id': recipe.id,
-            'name': recipe.name
+            'name': recipe.name,
+            'picklist': _picklist
         })
 
     # WE SERIALIZE AND RETURN LIST INSTEAD OF MODELS 
@@ -33,11 +38,17 @@ def get_recipe(id):
 
     _recipe = Recipe.query.get_or_404(id)
     if not _recipe:
-        abort(404)
+        abort(400)
     
+    _picklist = []
+    for pick in _recipe.recipe_pick:
+        _picklist.append(pick.id)
+
     return jsonify({
         'id': _recipe.id,
-        'username': _recipe.name})
+        'name': _recipe.name,
+        'picklist': _picklist
+        })
 
 
 @bp_api.route('/v1.0/recipes', methods=['POST'])
@@ -80,4 +91,40 @@ def create_recipe():
         'id': recipe.id,
         'name': recipe.name,
         'result': True
+    })
+
+
+# FOR TESTING
+
+# PICK ID, PIN
+LEDS = {
+    '1': 'Pin No.', '2': 'Pin No.', '3': 'Pin No.', '4': 'Pin No.',
+    '5': 'Pin No.','6': 'Pin No.', '7': 'Pin No.', '8': 'Pin No.',
+    '9': 'Pin No.', '10': 'Pin No.','11': 'Pin No.', '12': 'Pin No.',
+    '13': 'Pin No.', '14': 'Pin No.', '15': 'Pin No.','16': 'Pin No.',
+    '17': 'Pin No.', '18': 'Pin No.', '19': 'Pin No.', '20': 'Pin No.',
+    '21': 'Pin No.', '22': 'Pin No.', '23': 'Pin No.', '24': 'Pin No.',
+    '25': 'Pin No.','26': 'Pin No.', '27': 'Pin No.', '28': 'Pin No.',
+}
+
+@bp_api.route('/v1.0/ledsstatus', methods=['GET'])
+@auth.login_required
+def get_led_status():
+    import random
+    """ ENDPOINT: /api/v1.0/ledsstatus
+    """
+
+    _leds_status_list = []
+    
+    for id, pin in LEDS.items():
+        _status = random.randint(0,1)
+
+
+        _leds_status_list.append({
+            'id': id,
+            'status': _status,
+        })
+
+    return jsonify({
+        'leds_status': _leds_status_list
     })
