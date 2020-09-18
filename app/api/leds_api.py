62ea95ab003a13c9c8eb8467171f2078c2363e85
pythondev0101 <rmontemayor0101@gmail.com>
2020-09-18 08:43:27 +0800

    local update

--- app/api/leds_api.py
+++ app/api/leds_api.py
@@ -1,29 +1,8 @@
 from flask import (jsonify, abort, request, make_response)
 from app import auth, db
 from app.api import bp_api
-import RPi.GPIO as GPIO
 
 
-
-# ------------------------------------- RPI/LEDs CONFIGURATIONS ---------------------------------- #
-GPIO.setmode(GPIO.BOARD)
-
-# PICK ID, PIN
-LEDS = {
-    '1': 'Pin No.', '2': 'Pin No.', '3': 'Pin No.', '4': 'Pin No.',
-    '5': 'Pin No.','6': 'Pin No.', '7': 'Pin No.', '8': 'Pin No.',
-    '9': 'Pin No.', '10': 'Pin No.','11': 'Pin No.', '12': 'Pin No.',
-    '13': 'Pin No.', '14': 'Pin No.', '15': 'Pin No.','16': 'Pin No.',
-    '17': 'Pin No.', '18': 'Pin No.', '19': 'Pin No.', '20': 'Pin No.',
-    '21': 'Pin No.', '22': 'Pin No.', '23': 'Pin No.', '24': 'Pin No.',
-    '25': 'Pin No.','26': 'Pin No.', '27': 'Pin No.', '28': 'Pin No.',
-}
-
-# SETTING UP PINS TO OUT
-for id, pin in LEDS.items():
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
-
-# ------------------------------------- LEDs APIs ---------------------------------- #
 
 @bp_api.route('/v1.0/leds', methods=['GET'])
 @auth.login_required
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import os
 from app import create_app
-# import subprocess
 
 config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
